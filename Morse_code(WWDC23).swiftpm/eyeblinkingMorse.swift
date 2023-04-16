7ee724b7947a02c2b99a3d7e6dc73afb4b6b2c01
JungDohyeon <jdh1109ok@naver.com>
2023-04-16 15:39:01 +0900

    [Fix] Eye Detection

--- Morse_code(WWDC23).swiftpm/eyeblinkingMorse.swift
+++ Morse_code(WWDC23).swiftpm/eyeblinkingMorse.swift
@@ -1,85 +1,62 @@
-
 import SwiftUI
 import RealityKit
 import ARKit
-import Combine
 
 struct eyeblinkingMorse : View {
     var body: some View {
         ARViewContainer()
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
     func makeUIView(context: Context) -> ARView {
-        let arView = ARView(frame: .zero)
-        let eyeBlinkDetector = EyeBlinkDetector()
-        arView.session.delegate = eyeBlinkDetector
-        let config = ARFaceTrackingConfiguration()
-        config.isLightEstimationEnabled = true
-        arView.session.run(config)
-        context.coordinator.eyeBlinkDetector = eyeBlinkDetector
-        return arView
-    }
+           let arView = ARView(frame: .zero)
+           let config = ARFaceTrackingConfiguration()
+           config.isWorldTrackingEnabled = false // 카메라 전환을 위해 false로 설정
+           config.isLightEstimationEnabled = true
+           arView.session.run(config, options: [])
+           arView.session.delegate = context.coordinator // ARViewDelegate 지정
+           return arView
+       }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
     func makeCoordinator() -> Coordinator {
-        return Coordinator()
+        Coordinator()
     }
     
-    class Coordinator: NSObject {
-        var eyeBlinkDetector: EyeBlinkDetector?
-    }
-}
-
-class EyeBlinkDetector: NSObject, ARSessionDelegate {
-    
-    let eyeBlinkThreshold = 0.45
-    var blinkPublisher = PassthroughSubject<Void, Never>()
-    var leftEyeClosed = false
-    var rightEyeClosed = false
-    var closedStartTime: TimeInterval?
-    
-    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
-        guard let faceAnchor = anchors.first(where: { $0 is ARFaceAnchor }) as? ARFaceAnchor else { return }
+    class Coordinator: NSObject, ARSessionDelegate { // ARSessionDelegate 채택
+        var leftEyeBlinkValue: Float = 0
+        var rightEyeBlinkValue: Float = 0
+        var blinkStartedAt: Date?
+        var blinkEndedAt: Date?
         
-        let leftEyeBlinkValue = faceAnchor.blendShapes[.eyeBlinkLeft]?.doubleValue ?? 0
-        let rightEyeBlinkValue = faceAnchor.blendShapes[.eyeBlinkRight]?.doubleValue ?? 0
-        
-        if leftEyeBlinkValue > eyeBlinkThreshold {
-            leftEyeClosed = true
-        }
-        
-        if rightEyeBlinkValue > eyeBlinkThreshold {
-            rightEyeClosed = true
-        }
-        
-        
-        if leftEyeClosed && rightEyeClosed {
-            if closedStartTime == nil {
-                closedStartTime = CACurrentMediaTime()
-            }
-        }
-        
-        if leftEyeClosed && rightEyeClosed && leftEyeBlinkValue < 0.2 && rightEyeBlinkValue < 0.2 {
-            if closedStartTime == nil {
-                closedStartTime = CACurrentMediaTime()
-            }
+        func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+            guard let faceAnchor = anchors.first(where: { $0 is ARFaceAnchor }) as? ARFaceAnchor else { return }
             
-            if let closedStartTime = closedStartTime {
-                let blinkTime = CACurrentMediaTime() - closedStartTime
-                let _ = print(blinkTime)
-                
-                if blinkTime >= 0.1 {
-                    leftEyeClosed = false
-                    rightEyeClosed = false
-                    self.closedStartTime = nil
-                    blinkPublisher.send(())
+            // 눈 깜빡임 감지
+            leftEyeBlinkValue = faceAnchor.blendShapes[.eyeBlinkLeft]?.floatValue ?? 0.0
+            rightEyeBlinkValue = faceAnchor.blendShapes[.eyeBlinkRight]?.floatValue ?? 0.0
+            
+            if leftEyeBlinkValue > 0.4 || rightEyeBlinkValue > 0.4 {
+                if blinkStartedAt == nil {
+                    blinkStartedAt = Date()
+                }
+            } else {
+                if blinkStartedAt != nil {
+                    blinkEndedAt = Date()
+                    if let startedAt = blinkStartedAt, let endedAt = blinkEndedAt {
+                        let blinkDuration = endedAt.timeIntervalSince(startedAt)
+                        let _ = print("Blink duration: \(blinkDuration) seconds")
+                        // 눈 감았다 뜬 시간 측정 기능 추가
+                        let closedDuration = endedAt.timeIntervalSince(startedAt)
+                        let _ = print("Eyes closed for \(closedDuration) seconds")
+                    }
+                    blinkStartedAt = nil
+                    blinkEndedAt = nil
                 }
             }
-        } else {
-            closedStartTime = nil
         }
     }
 }
