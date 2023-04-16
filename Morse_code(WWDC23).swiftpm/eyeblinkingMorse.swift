a3f3b5b88064444a2c7e5ef7513d09525cbcbae5
JungDohyeon <jdh1109ok@naver.com>
2023-04-16 13:06:36 +0900

    [Fix] eye detection

--- Morse_code(WWDC23).swiftpm/eyeblinkingMorse.swift
+++ Morse_code(WWDC23).swiftpm/eyeblinkingMorse.swift
@@ -1,57 +1,28 @@
+
 import SwiftUI
+import RealityKit
 import ARKit
 import Combine
 
-class EyeBlinkDetector: NSObject, ARSCNViewDelegate {
-    
-    var blinkPublisher = PassthroughSubject<Void, Never>()
-    var leftEyeClosed: Bool = false
-    var rightEyeClosed: Bool = false
-    
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        guard anchor is ARFaceAnchor else { return nil }
-        let contentNode = SCNNode()
-        return contentNode
-    }
-    
-    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        guard let faceAnchor = anchor as? ARFaceAnchor else { return }
-        
-        // 양쪽 눈 미는 정도를 가져옴
-        let leftEyeBlinkValue = faceAnchor.blendShapes[.eyeBlinkLeft]?.doubleValue ?? 0
-        let rightEyeBlinkValue = faceAnchor.blendShapes[.eyeBlinkRight]?.doubleValue ?? 0
-        
-        // 눈이 완전히 감겼는지 체크
-        if leftEyeBlinkValue > 0.4 {
-            leftEyeClosed = true
-            let _ = print("left eye blinking")
-        }
-        if rightEyeBlinkValue > 0.4 {
-            rightEyeClosed = true
-            let _ = print("right eye blinking")
-        }
-        
-        // 눈이 완전히 감겨있는 상태에서 눈을 떴을 때, blinkPublisher에서 Void값을 emit
-        if leftEyeClosed && rightEyeClosed && leftEyeBlinkValue < 0.2 && rightEyeBlinkValue < 0.2 {
-            leftEyeClosed = false
-            rightEyeClosed = false
-            blinkPublisher.send(())
-        }
+struct eyeblinkingMorse : View {
+    var body: some View {
+        ARViewContainer()
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
-    func makeUIView(context: Context) -> ARSCNView {
-        let arView = ARSCNView(frame: .zero)
+    func makeUIView(context: Context) -> ARView {
+        let arView = ARView(frame: .zero)
         let eyeBlinkDetector = EyeBlinkDetector()
-        arView.delegate = eyeBlinkDetector
-        let configuration = ARFaceTrackingConfiguration()
-        arView.session.run(configuration)
+        arView.session.delegate = eyeBlinkDetector
+        let config = ARFaceTrackingConfiguration()
+        config.isLightEstimationEnabled = true
+        arView.session.run(config)
         context.coordinator.eyeBlinkDetector = eyeBlinkDetector
         return arView
     }
     
-    func updateUIView(_ uiView: ARSCNView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {}
     
     func makeCoordinator() -> Coordinator {
         return Coordinator()
@@ -62,11 +33,53 @@ struct ARViewContainer: UIViewRepresentable {
     }
 }
 
-struct eyeblinkingMorse: View {
-    var body: some View {
-        VStack {
-            ARViewContainer()
-            Text("Blink your eyes to emit the event")
+class EyeBlinkDetector: NSObject, ARSessionDelegate {
+    
+    let eyeBlinkThreshold = 0.45
+    var blinkPublisher = PassthroughSubject<Void, Never>()
+    var leftEyeClosed = false
+    var rightEyeClosed = false
+    var closedStartTime: TimeInterval?
+    
+    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        guard let faceAnchor = anchors.first(where: { $0 is ARFaceAnchor }) as? ARFaceAnchor else { return }
+        
+        let leftEyeBlinkValue = faceAnchor.blendShapes[.eyeBlinkLeft]?.doubleValue ?? 0
+        let rightEyeBlinkValue = faceAnchor.blendShapes[.eyeBlinkRight]?.doubleValue ?? 0
+        
+        if leftEyeBlinkValue > eyeBlinkThreshold {
+            leftEyeClosed = true
+        }
+        
+        if rightEyeBlinkValue > eyeBlinkThreshold {
+            rightEyeClosed = true
+        }
+        
+        
+        if leftEyeClosed && rightEyeClosed {
+            if closedStartTime == nil {
+                closedStartTime = CACurrentMediaTime()
+            }
+        }
+        
+        if leftEyeClosed && rightEyeClosed && leftEyeBlinkValue < 0.2 && rightEyeBlinkValue < 0.2 {
+            if closedStartTime == nil {
+                closedStartTime = CACurrentMediaTime()
+            }
+            
+            if let closedStartTime = closedStartTime {
+                let blinkTime = CACurrentMediaTime() - closedStartTime
+                let _ = print(blinkTime)
+                
+                if blinkTime >= 0.1 {
+                    leftEyeClosed = false
+                    rightEyeClosed = false
+                    self.closedStartTime = nil
+                    blinkPublisher.send(())
+                }
+            }
+        } else {
+            closedStartTime = nil
         }
     }
 }
